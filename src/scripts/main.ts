09f442d76c139418ac756f998b03904e4462ea65
NoamRa <noamraby@gmail.com>
2020-01-15 23:54:00 +0200

    mvp loading image from input to canvas

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,10 +1,47 @@
-function component() {
-  const element = document.createElement("div");
+const HIDDEN_IMAGE_ID = "hidden-image";
+const CANVAS_ID = "canvas";
+const INPUT_ID = "image-picker";
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = ["Hello", "webpack"].join(" ");
-
-  return element;
+function getHiddenImage(): HTMLImageElement {
+  return document.getElementById(HIDDEN_IMAGE_ID) as HTMLImageElement;
 }
 
-document.body.appendChild(component());
+function getCanvas(): HTMLCanvasElement {
+  return document.getElementById(CANVAS_ID) as HTMLCanvasElement;
+}
+
+function storeImage(image: File) {
+  const imageEl = getHiddenImage();
+  imageEl.src = window.URL.createObjectURL(image);
+}
+
+function loadImageToCanvas(imageFile: File) {
+  let image: HTMLImageElement;
+  let imageReader: FileReader;
+
+  function createImage() {
+    image = new Image();
+    image.onload = imageLoaded;
+    image.src = imageReader.result as string;
+  }
+
+  function imageLoaded() {
+    const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+    canvas.width = image.width;
+    canvas.height = image.height;
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(image, 0, 0);
+  }
+
+  imageReader = new FileReader();
+  imageReader.onload = createImage;
+  imageReader.readAsDataURL(imageFile);
+}
+
+export function handleImageChange(fileInput: HTMLInputElement) {
+  const file: File = fileInput.files[0];
+  if (file) {
+    storeImage(file);
+    loadImageToCanvas(file);
+  }
+}
